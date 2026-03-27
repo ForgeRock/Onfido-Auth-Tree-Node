b7437f6d31b849a0e042e89736256143e2c5f477
TylerGelinas <tyler.gelinas@forgerock.com>
2026-03-27 15:19:52 -0500

    TNTP-264: Onfido corewrapper compiler update

--- src/main/java/com/onfido/onfidoRegistrationNode/onfidoRegistrationNodePlugin.java
+++ src/main/java/com/onfido/onfidoRegistrationNode/onfidoRegistrationNodePlugin.java
@@ -60,7 +60,7 @@ import java.util.Map;
  */
 public class onfidoRegistrationNodePlugin extends AbstractNodeAmPlugin {
 
-    static private String currentVersion = "2.4.2";
+    static private String currentVersion = "2.4.3";
     static final String logAppender = "[Version: " + currentVersion + "][Marketplace] ";
 
     /**
